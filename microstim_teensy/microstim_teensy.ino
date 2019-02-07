2914981f5b755edec59d3d2e3442edee471925f4
natecermak <cerman07@protonmail.com>
2019-02-07 11:47:12 +0200

    almost done with revision 3. 12v regs removed, GPIO breakout and indicator LEDs added, layout cleaned up

--- microstim_teensy/microstim_teensy.ino
+++ microstim_teensy/microstim_teensy.ino
@@ -10,7 +10,6 @@
 #define MICROAMPS_PER_ADC 0.2325            // 20V * (1/(10500 V/A)) / 2^13 = 0.2325
 #define MILLIVOLTS_PER_DAC 5.81             // 5V / 2^12 * gain of 4.758 ~= 6 mV / DAC unit
 #define MILLIVOLTS_PER_ADC 2.44             // 20V / 2^13 = 2.44 mV / ADC unit
-#define SLEW_FUDGE 10                       // microseconds
 #define OE0 6
 #define OE1 5
 #define OE2 4
@@ -19,7 +18,7 @@
 #define CS1 7
 #define NLDAC 9
 #define PT_ARRAY_LENGTH 10
-#define MAX_NUM_PHASES 10
+#define MAX_NUM_STAGES 10
 
 
 // ------------- SPI setup ------------------------------------- //
@@ -42,13 +41,13 @@ struct PulseTrain {
   unsigned int period;                          // usec
   unsigned long duration;                       // usec
 
-  int nPhases;
-  int amplitude[2][MAX_NUM_PHASES];             // mV or uA, depending on mode
-  unsigned int phaseDuration[MAX_NUM_PHASES];   // usec
+  int nStages;
+  int amplitude[2][MAX_NUM_STAGES];             // mV or uA, depending on mode
+  unsigned int stageDuration[MAX_NUM_STAGES];   // usec
 
   unsigned long trainStartTime;                 // usec 
   int nPulses;
-  int measuredAmplitude[2][MAX_NUM_PHASES];
+  int measuredAmplitude[2][MAX_NUM_STAGES];
 };
 volatile PulseTrain PTs[PT_ARRAY_LENGTH];
 volatile PulseTrain *activePT0, *activePT1;
@@ -246,10 +245,10 @@ int pulse (volatile PulseTrain* PT) {
 
   if (micros() - PT->trainStartTime >= PT->duration) {
     Serial.print("Train complete. Delivered "); Serial.print(PT->nPulses); 
-    Serial.println(" pulses.\nCurrent/Voltage by phase: ");
+    Serial.println(" pulses.\nCurrent/Voltage by stage: ");
     char str[100];
-    for (int i = 0; i < PT->nPhases; i++) {
-      sprintf(str, "Phase %d: %6d%s, %6d%s,    (target %6d%s, %6d%s)\n", i,
+    for (int i = 0; i < PT->nStages; i++) {
+      sprintf(str, "Stage %d: %6d%s, %6d%s,    (target %6d%s, %6d%s)\n", i,
               PT->measuredAmplitude[0][i] / PT->nPulses,
               (PT->mode[0] < 2)?"mV":"uA",
               PT->measuredAmplitude[1][i] / PT->nPulses,
@@ -267,11 +266,11 @@ int pulse (volatile PulseTrain* PT) {
   setOutputMode(0, PT->mode[0]);
   setOutputMode(1, PT->mode[1]);
 
-  for (int i = 0; i < PT->nPhases; i++) {
+  for (int i = 0; i < PT->nStages; i++) {
     writeToDacs(PT->amplitude[0][i] / ((!PT->mode[0])?MILLIVOLTS_PER_DAC:MICROAMPS_PER_DAC) + ((PT->mode[0])?currentOffsets[0]:voltageOffsets[0]), //* (PT->mode[0] != 3),
                 PT->amplitude[1][i] / ((!PT->mode[1])?MILLIVOLTS_PER_DAC:MICROAMPS_PER_DAC) + ((PT->mode[1])?currentOffsets[1]:voltageOffsets[1])); //* (PT->mode[1] != 3));
 
-    delayMicroseconds(max( ((long int) PT->phaseDuration[i]) - SLEW_FUDGE, 0));
+    delayMicroseconds(max( ((long int) PT->stageDuration[i]), 0));
     
     int lastAdcRead[2];
     readADC(lastAdcRead); // note: this limits bandwidth for voltage pulses
@@ -319,9 +318,9 @@ void printPulseTrainParameters(int i){
     PTs[i].period, 0.000001*PTs[i].period, 1000000.0/PTs[i].period, PTs[i].duration, 0.000001*PTs[i].duration);
   Serial.print(str);
   
-  Serial.println("\n  phase    duration     output0   output1");
-  for (int j = 0; j< PTs[i].nPhases; j++){
-    sprintf(str, "   %2d  %7d usec %8d%s %8d%s\n", j, PTs[i].phaseDuration[j],
+  Serial.println("\n  stage    duration     output0   output1");
+  for (int j = 0; j< PTs[i].nStages; j++){
+    sprintf(str, "   %2d  %7d usec %8d%s %8d%s\n", j, PTs[i].stageDuration[j],
             PTs[i].amplitude[0][j], (PTs[i].mode[0] == 0)?"mV":"uA",
             PTs[i].amplitude[1][j], (PTs[i].mode[1] == 0)?"mV":"uA");
     Serial.print(str);
@@ -333,7 +332,7 @@ void printPulseTrainParameters(int i){
 
 volatile PulseTrain* clearPulseTrainHistory(volatile PulseTrain* PT){
   PT->nPulses = 0;
-  for (int i = 0; i < PT->nPhases; i++){
+  for (int i = 0; i < PT->nStages; i++){
     PT->measuredAmplitude[0][i] = 0;
     PT->measuredAmplitude[1][i] = 0;
   }
@@ -386,20 +385,20 @@ void setup() {
     PTs[i].mode[1] = 0;
     PTs[i].period = 1000;
     PTs[i].duration = 10000000;
-    PTs[i].nPhases = 2;
-    for (int j = 0; j < MAX_NUM_PHASES; j++){
+    PTs[i].nStages = 2;
+    for (int j = 0; j < MAX_NUM_STAGES; j++){
       PTs[i].amplitude[0][j] = j?-1000:1000;
       PTs[i].amplitude[1][j] = j?1000:-1000;
-      PTs[i].phaseDuration[j] = 100;
+      PTs[i].stageDuration[j] = 100;
     }
   } 
 
   PTs[1].mode[0] = 2;
   PTs[1].mode[1] = 2;
-  for (int j = 0; j < MAX_NUM_PHASES; j++){
+  for (int j = 0; j < MAX_NUM_STAGES; j++){
     PTs[1].amplitude[0][j] = j?-100:100;
     PTs[1].amplitude[1][j] = j?100:1-00;
-    PTs[1].phaseDuration[j] = 100;
+    PTs[1].stageDuration[j] = 100;
   }
 
 
@@ -436,18 +435,18 @@ void loop() {
                &(PTs[ptIndex].period), 
                &(PTs[ptIndex].duration));
         if (m==4){
-          PTs[ptIndex].nPhases=0;
+          PTs[ptIndex].nStages=0;
           char *token = strtok(comBuf+1, ";");
           token = strtok(NULL, ";"); //move to the 2nd segment delimited by ";"
           
           while (token != NULL){
              m = sscanf(token, "%d,%d,%u", 
-                       &(PTs[ptIndex].amplitude[0][PTs[ptIndex].nPhases]),
-                       &(PTs[ptIndex].amplitude[1][PTs[ptIndex].nPhases]),
-                       &(PTs[ptIndex].phaseDuration[PTs[ptIndex].nPhases]));
+                       &(PTs[ptIndex].amplitude[0][PTs[ptIndex].nStages]),
+                       &(PTs[ptIndex].amplitude[1][PTs[ptIndex].nStages]),
+                       &(PTs[ptIndex].stageDuration[PTs[ptIndex].nStages]));
              if (m != 3)
                 break;
-             PTs[ptIndex].nPhases++;
+             PTs[ptIndex].nStages++;
              token = strtok(NULL, ";");
           }
         }
@@ -475,7 +474,7 @@ void loop() {
       }
       
       else if (comBuf[0] == 'D') {
-        sscanf(comBuf + 1, "%d,%d", &(currentOffsets[0]), &(currentOffsets[1]) );
+        //sscanf(comBuf + 1, "%d,%d", &(currentOffsets[0]), &(currentOffsets[1]) );
         Serial.print("currentOffsets[0]: "); Serial.println(currentOffsets[0]);
         Serial.print("currentOffsets[1]: "); Serial.println(currentOffsets[1]);
       }
