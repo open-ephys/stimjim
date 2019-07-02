42e2c8964d78ae8847323e2884d3db4cb8952bda
Nate Cermak <cerman07@protonmail.com>
2019-07-02 14:47:13 +0300

    made sure adc measurements during pulses account for adc offset

--- stimjimPulser/stimjimPulser.ino
+++ stimjimPulser/stimjimPulser.ino
@@ -84,9 +84,9 @@ int pulse (volatile PulseTrain* PT) {
 
     // read ADCs
     if (PT->mode[0] < 2)
-      PT->measuredAmplitude[0][i] += Stimjim.readAdc(0, PT->mode[0] > 0) * ((PT->mode[0]) ? MICROAMPS_PER_ADC : MILLIVOLTS_PER_ADC);
+      PT->measuredAmplitude[0][i] += (Stimjim.readAdc(0, PT->mode[0] > 0)-Stimjim.adcOffset10[0]) * ((PT->mode[0]) ? MICROAMPS_PER_ADC : MILLIVOLTS_PER_ADC);
     if (PT->mode[1] < 2)
-      PT->measuredAmplitude[1][i] += Stimjim.readAdc(1, PT->mode[1] > 0) * ((PT->mode[1]) ? MICROAMPS_PER_ADC : MILLIVOLTS_PER_ADC);
+      PT->measuredAmplitude[1][i] += (Stimjim.readAdc(1, PT->mode[1] > 0)-Stimjim.adcOffset10[1]) * ((PT->mode[1]) ? MICROAMPS_PER_ADC : MILLIVOLTS_PER_ADC);
 
     if ( i + 1 < PT->nStages) {
       dac0val = PT->amplitude[0][i + 1] / ((!PT->mode[0]) ? MILLIVOLTS_PER_DAC : MICROAMPS_PER_DAC) + ((PT->mode[0]) ? Stimjim.currentOffsets[0] : Stimjim.voltageOffsets[0]);
@@ -262,17 +262,6 @@ void setup() {
   IT1.priority(64);
   Serial.flush();
   Serial.println("Ready to go!\n\n");
-
-
-  Serial.print("Stimjim adcOffset25 = ");
-  Serial.print(Stimjim.adcOffset25[0]); Serial.print(" , ");
-  Serial.println(Stimjim.adcOffset25[1]);
-
-  Serial.print("Stimjim adcOffset10 = ");
-  Serial.print(Stimjim.adcOffset10[0]); Serial.print(" , ");
-  Serial.println(Stimjim.adcOffset10[1]);
-  
-
 }
 
 
